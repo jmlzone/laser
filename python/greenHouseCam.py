0bbf70fbfbc397d390a3b0dcea763f2560d47049
James Lee <jml@jmlzone.com>
2019-07-06 21:43:01 -0400

    add spacer

--- python/greenHouseCam.py
+++ python/greenHouseCam.py
@@ -4,7 +4,7 @@ from svgwrite.data.types import SVGAttribute
 from svgwrite.extensions import Inkscape
 import math
 
-dwg = svgwrite.Drawing('greenHouseCam.svg', profile='full', size=('215mm','50mm'), viewBox="0 0 215 50")
+dwg = svgwrite.Drawing('greenHouseCam.svg', profile='full', size=('215mm','100mm'), viewBox="0 0 215 100")
 ink=Inkscape(dwg)
 
 def greenHouseCamBlank(dwg, ox,oy, camOD=35.0, camID=10.0, mhd=3.5,mho=5.5) :
@@ -49,20 +49,20 @@ def greenHouseCamCalc(dwg, ox,oy, camOD=35.0, camID=10.0, mhd=3.5,mho=5.5,offDis
     rlaf = (remAngle>180)
     if(rlaf<0) :
         print("error remining angle less than zero")
-    print("remaining angle =%f" %remAngle)
+    #print("remaining angle =%f" %remAngle)
     rise=onDisp-offDisp
     dwellRad = math.radians(dwellAngle/2.0)
     yOff=offDisp * math.sin(dwellRad)
     xOff=offDisp * math.cos(dwellRad)
-    print("dwell agle = %f, %f, dlaf=%d" %(dwellAngle,dwellRad,dlaf))
-    print("offset X,Y %f,%f" %(xOff, yOff))
+    #print("dwell agle = %f, %f, dlaf=%d" %(dwellAngle,dwellRad,dlaf))
+    #print("offset X,Y %f,%f" %(xOff, yOff))
     remRad = math.radians(remAngle/2.0)
     ryOff=onDisp * math.sin(remRad)
     rxOff=onDisp * math.cos(remRad)
     cpx = (xOff-rxOff)/2.0
     cpy = (yOff+ryOff)/2.0
     cpy = ryOff
-    print("Control point %f,%f" % (cpx,cpy)) 
+    #print("Control point %f,%f" % (cpx,cpy)) 
     # move to start
     pathText = "M%f,%f\n" % ((cx+xOff),(cy-yOff))
     #dwell off arc
@@ -81,14 +81,60 @@ def greenHouseCamCalc(dwg, ox,oy, camOD=35.0, camID=10.0, mhd=3.5,mho=5.5,offDis
     dwg.add(p)
     return()
 
-#greenHouseCamBlank(dwg,5,5)
+def greenHouseSpacer(dwg, ox,oy, spacerOD=26.0, camID=10.0, mhd=3.5,mho=5.5,notch=4) :
+    cx=ox+(spacerOD/2.0)
+    cy=oy+(spacerOD/2.0)
+    nw=notch/2.0
+    r1=camID/2.0
+    r2=spacerOD/2.0
+    th1=math.asin(nw/r1)
+    th2=math.asin(nw/r2)
+    x1=math.cos(th1) * r1
+    x2=math.cos(th2) * r2
+    #print("R1=%f x1=%f" %(r1, x1))
+    #print("R2=%f x2=%f" %(r2, x2))
+    # inner cicle
+    #c=dwg.add(dwg.circle(center=(cx,cy), r=(camID/2.0), stroke="black", fill="none", stroke_width=0.1))
+    # mounting holes top only
+#    c=dwg.add(dwg.circle(center=(cx+mho,cy+mho), r=(mhd/2.0), stroke="black", fill="none", stroke_width=0.1))
+    c=dwg.add(dwg.circle(center=(cx+mho,cy-mho), r=(mhd/2.0), stroke="black", fill="none", stroke_width=0.1))
+#    c=dwg.add(dwg.circle(center=(cx-mho,cy+mho), r=(mhd/2.0), stroke="black", fill="none", stroke_width=0.1))
+    c=dwg.add(dwg.circle(center=(cx-mho,cy-mho), r=(mhd/2.0), stroke="black", fill="none", stroke_width=0.1))
+    # outer circle conver to path when cam shape is figured out
+    #c=dwg.add(dwg.circle(center=(cx,cy), r=(spacerOD/2.0), stroke="black", fill="none", stroke_width=0.1))
+    # move to start
+    pathText = "M%f,%f\n" % ((cx+x1),(cy-nw))
+    #ID half arc
+    pathText = pathText+ "A%f,%f 0 0 0 %f,%f\n" %(r1,r1,(cx-x1),(cy-nw))
+    pathText = pathText+ "L%f,%f\n" %((cx-x2),(cy-nw))
+    pathText = pathText+ "A%f,%f 0 %d 1 %f,%f\n" %(r2,r2,0,(cx+x2),(cy-nw))
+    pathText = pathText+ "L%f,%f\n" %((cx+x1),(cy-nw))
+    p=dwg.path(d="%s" %pathText,
+    stroke="black", fill="none", stroke_width=0.1)
+    dwg.add(p)
+    return()
+sx=2
+sy=2
+ox=27
+oy=11
+for x in list(range(sx, (2*ox),ox)) :
+    for y in list(range(sy, (8*oy), oy)) :
+        greenHouseSpacer(dwg,x,y)
+
+#greenHouseSpacer(dwg,5,5)
+#greenHouseSpacer(dwg,5,16)
+#greenHouseSpacer(dwg,5,27)
+
+#greenHouseSpacer(dwg,32,5)
+#greenHouseSpacer(dwg,32,16)
+#greenHouseSpacer(dwg,32,27)
 #
 #greenHouseCamCalc(dwg,5,5)
 #greenHouseCamCalc(dwg,45,5)
-greenHouseCamCalc(dwg,78,5)
-greenHouseCamCalc(dwg,111,5)
-greenHouseCamCalc(dwg,144,5)
-greenHouseCamCalc(dwg,177,5)
+#greenHouseCamCalc(dwg,78,5)
+#greenHouseCamCalc(dwg,111,5)
+#greenHouseCamCalc(dwg,144,5)
+#greenHouseCamCalc(dwg,177,5)
 dwg.save(pretty=True)
 # -------------------> +X
 # arc
